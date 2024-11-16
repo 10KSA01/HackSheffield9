4ecf2a454c7dcdaed3e73b5c3784d2b0b8f091f1
10KSA01 <kumaracharjee@outlook.com>
2024-11-16 19:57:43 +0000

    best project ever

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+# App title and description
+st.title("Crypto-Powered Gamified Learning Platform")
+st.subheader("Learn skills, complete challenges, and earn rewards!")
+
+yt_vid = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+
+st.video(yt_vid)
